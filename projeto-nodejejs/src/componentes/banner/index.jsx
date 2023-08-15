59e219676b77add9b23857cf20da80032b4d8643
André Luiz Lessa <49814751+andrelessssa@users.noreply.github.com>
2023-08-15 11:59:09 -0300

    banner
    criando banner

---
+++ projeto-nodejejs/src/componentes/banner/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../../assets/Header@2x.png";
+
+    
+function Banner  ()  {
+    return(
+        <div>
+          
+          <img src="./src/assets/HeaderHeader_total.png" alt="" />          
+        </div>
+        
+    )
+}
+
+
+export default Banner;
